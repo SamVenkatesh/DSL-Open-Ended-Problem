a1b010e2608e0e15180a8bb00a93c5741097fb1a
Sam <vsamster95@gmail.com>
2014-11-16 19:32:59 +0530

    First Commit, functions prtotyped, subject to change

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+FILE fnCreateDB(char []);
+void fnAddRecord();
+void fnDispAllDB();
+void fnDispDB();
+void fnOpenDB(FILE *, char []);
+void fnSearch();
+void fnChooseDType();
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int iChoice,iVal;
+    char DbName[25];
+	FILE *fp;
+	for(;;)
+	{
+        printf("\nApplication Operations\n");
+        printf("\n1.Create new Database\n2.Display all databases\n3.Exit\n");
+        scanf("%d",&iChoice);
+        switch(iChoice)
+        {
+            case 1: printf("Enter the Database name\n");
+                    scanf("%s",DbName);
+                    fp=fnCreateDB(DbName);
+                    fnOpenDB(fp,DbName);
+                    break;
+            case 2: fnDispAllDB();
+                    break;
+            case 3: exit(0);
+            default:printf("\nInvalid Operation\n");
+
+        }
+	}
+
 }
+
+void fnAddRecord()
+{
+
+}
+
+FILE fnCreateDB(char DbName[])
+{
+    FILE *fp;
+    fp=(DbName,"w");
+
+}
+
